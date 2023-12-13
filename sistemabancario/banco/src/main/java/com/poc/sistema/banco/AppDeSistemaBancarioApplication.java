f31a9aaa7ed86201087a3612fbf9ccae1935ec38
FXOliveira <81167481+FXOliveira@users.noreply.github.com>
2023-12-13 07:54:19 -0300

    Add files via upload

---
+++ sistemabancario/banco/src/main/java/com/poc/sistema/banco/AppDeSistemaBancarioApplication.java
@@ -0,0 +1,13 @@
+package com.poc.sistema.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDeSistemaBancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDeSistemaBancarioApplication.class, args);
+	}
+
+}
